13c566795c87ef4a2db23d1adf7517c4e4efd86f
CamiloArias12 <ariaspira13@gmail.com>
2023-08-14 11:24:02 -0500

    initial commit

--- src/modules/parameterization/thirds/location/country/country.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CountryResolver } from './country.resolver';
-
-@Module({
-  providers: [CountryResolver]
-})
-export class CountryModule {}
--- src/modules/parameterization/thirds/location/town/town.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TownService } from './town.service';
-import { TownResolver } from './town.resolver';
-
-@Module({
-  providers: [TownService, TownResolver]
-})
-export class TownModule {}
