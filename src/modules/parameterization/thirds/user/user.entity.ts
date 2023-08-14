96f53a97b6d4b6aa877095c44614e7b7636984c9
RulosSolitario <ronaldgarcia999@gmail.com>
2023-08-14 16:12:18 -0500

    add parametres users

--- src/modules/parameterization/thirds/user/user.entity.ts
+++ src/modules/parameterization/thirds/user/user.entity.ts
@@ -7,12 +7,68 @@ import { Column, Entity, PrimaryColumn, TableInheritance } from "typeorm";
 @TableInheritance({ column: { type: "varchar", name: "type" } })
 export   abstract class User{
 
+   @Field()   
+   @Column()
+   tipoIdentificacion: string
+
    @Field()
-   @PrimaryColumn()
-   identification:number;
+   @PrimaryColumn({unique: true})
+   numeroIdentificacion: number
 
    @Field()
    @Column()
-   name:string
+   nombre: string
+
+   @Field()
+   @Column()
+   sueldo: string
+
+   @Field()
+   @Column()
+   otrosIngresos: string
+
+   @Field()
+   @Column()
+   direccionResidencia: string
+
+   @Field()
+   @Column()
+   telefono: string
+
+   @Field()
+   @Column()
+   correo: string
+
+   @Field()
+   @Column()
+   fechaNacimiento: Date;
+
+   @Field()
+   @Column()
+   pais: string;
+
+   @Field()
+   @Column()
+   municipio: Date
+
+   @Field()
+   @Column()
+   estado: string
+
+   @Field()
+   @Column()
+   fechaAfiliacion: Date
+
+   @Field()
+   @Column()
+   fechaRetiro: Date
+
+   @Field()
+   @Column()
+   escolaridad: string
+
+   @Field()
+   @Column()
+   genero: string
 
 }
