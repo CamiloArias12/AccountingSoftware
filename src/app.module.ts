f8dab47bc29f952b4b73818cf3e30c84a63e1fd9
RulosSolitario <ronaldgarcia999@gmail.com>
2023-08-14 15:52:08 -0500

    inicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './User/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql', 
+    host: 'localhost', 
+    port: 3306, 
+    username: 'root', 
+    password: '9898', 
+    database: 'users',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
