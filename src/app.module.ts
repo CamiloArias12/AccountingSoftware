658e8bc6f50d6031eb2edfe18be38f5c796d75a3
CamiloArias12 <ariaspira13@gmail.com>
2023-08-21 23:23:24 -0500

    add database and enviroment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,45 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ParameterizationModule } from './modules/parameterization/parameterization.module';
 import { join } from 'path';
 import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Affiliate } from './modules/parameterization/thirds/affiliate/affiliate.entity';
+import { User } from './modules/parameterization/thirds/user/user.entity';
+import { configDB } from './configuration';
+import { Employee } from './modules/parameterization/thirds/employee/employee.entity';
+import { Beneficiary } from './modules/parameterization/thirds/beneficiary/beneficiary.entity';
+import { BeneficiaryAffiliate } from './modules/parameterization/thirds/beneficiary-affiliate/beneficiary-affiliate.entity';
 
 @Module({
+  
   imports: [ParameterizationModule,
       GraphQLModule.forRoot(
 	 {
 	    driver:ApolloDriver,
 	    autoSchemaFile:join(process.cwd(),'src/schema.gql')
 	 }
+      ),
+
+   ConfigModule.forRoot({
+	    envFilePath:'.env',
+	    isGlobal:true,
+	 }
+      ),
+      TypeOrmModule.forRoot(
+	 {
+	    type: 'mysql',
+	    host: process.env.HOST,
+	    port: parseInt(process.env.PORT),
+	    username: process.env.DATABASE_USER,
+	    password: process.env.DATABASE_PASSWORD,
+	    database: process.env.DATABASE_NAME,
+	    keepConnectionAlive: true,
+	    synchronize: true,
+	    entities:[User,Affiliate,Employee,Beneficiary,BeneficiaryAffiliate]
+	 }
       )
-  ],
+    ],
+  providers: [AppService ]
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+   constructor (private readonly configService:ConfigService){}
+
+
+   getSecretKey():string{
+      return this.configService.get('API_KEY');
+   }
 }
