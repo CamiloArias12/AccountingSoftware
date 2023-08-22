39f75c48998515f931b407ddb27d780dcc3fbb77
CamiloArias12 <ariaspira13@gmail.com>
2023-08-21 23:25:22 -0500

    add employee module

--- src/modules/parameterization/thirds/employee/employee.entity.ts
+++ src/modules/parameterization/thirds/employee/employee.entity.ts
@@ -1,11 +1,17 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { Column, Entity } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../user/user.entity";
+import { IEmployee } from "./dto/employee.interface";
 
 
 @ObjectType()
 @Entity()
-export class Employee{ 
+export class Employee implements IEmployee{ 
    
+   @PrimaryGeneratedColumn()
+   idEmployee:number
+
+
    @Field()
    @Column()
    username:string
@@ -14,5 +20,9 @@ export class Employee{
    @Column()
    password:string
 
+
+   @OneToOne(() => User ,user => user.employee)
+   @JoinColumn({name: "idEmployee"})
+   user:User
 }
 
--- src/modules/parameterization/thirds/employee/employee.module.ts
+++ src/modules/parameterization/thirds/employee/employee.module.ts
@@ -3,9 +3,10 @@ import { EmployeeService } from './employee.service';
 import { EmployeeResolver } from './employee.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-   imports:[TypeOrmModule.forFeature([Employee])],
+   imports:[TypeOrmModule.forFeature([Employee]),UserModule],
 
    providers: [EmployeeService, EmployeeResolver]
 })
