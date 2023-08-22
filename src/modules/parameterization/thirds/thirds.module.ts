7045b3260a82562eac6e019a59c9c8ce1a143dee
CamiloArias12 <ariaspira13@gmail.com>
2023-08-21 23:26:32 -0500

    import modules

--- src/modules/parameterization/thirds/thirds.module.ts
+++ src/modules/parameterization/thirds/thirds.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LocationModule } from './location/location.module';
-import { UserModule } from './user/user.module';
+import { AffiliateModule } from './affiliate/affiliate.module';
+import { EmployeeModule } from './employee/employee.module';
+import { BeneficiaryAffiliateService } from './beneficiary-affiliate/beneficiary-affiliate.service';
+import { BeneficiaryAffiliateModule } from './beneficiary-affiliate/beneficiary-affiliate.module';
+import { AuthService } from './auth/auth.service';
+import { AuthResolver } from './auth/auth.resolver';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-   imports:[LocationModule, UserModule]
+   imports:[LocationModule,AffiliateModule,EmployeeModule, BeneficiaryAffiliateModule, AuthModule],
+   providers: [BeneficiaryAffiliateService, AuthService, AuthResolver]
 
 })
 export class ThirdsModule {}
