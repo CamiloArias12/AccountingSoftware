8990d7cd4d04be63bc0ed8e707608915df4e6d9d
CamiloArias12 <ariaspira13@gmail.com>
2023-10-07 09:42:04 -0500

    add lib type saving

---
+++ packages/web/src/lib/utils/type-saving/types.ts
@@ -0,0 +1,13 @@
+import { TypeAccounnt } from "../type-account/types"
+
+export type TypeSaving= {
+   id:number
+   name:string
+   auxiliarys:Auxiliary[]
+   
+
+}
+
+export type Auxiliary ={
+   typeAccount:TypeAccounnt
+}
