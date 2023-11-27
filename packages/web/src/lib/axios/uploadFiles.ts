794e74e8be2d9cb2196ea010101a56dc39af6d53
CamiloArias12 <ariaspira13@gmail.com>
2023-11-27 13:10:06 -0500

    Add uploadFiles

--- packages/web/src/lib/axios/uploadFiles.ts
+++ packages/web/src/lib/axios/uploadFiles.ts
@@ -13,3 +13,77 @@ export async function uploadFileAccounts(formData: FormData): Promise<Boolean> {
     return false;
   }
 }
+
+
+export async function downloadPuc(): Promise<Boolean> {
+  try {
+    const respose = await axios({
+      method: 'get',
+      url: `${process.env.API_ENDPOINT}/type-account/download`,
+      responseType:'blob'
+    });
+    const url=window.URL.createObjectURL(new Blob([respose.data]))
+    const link=document.createElement('a')
+    link.href=url
+    link.setAttribute('download',"puc.xlsx")
+    link.click()
+    document.body.removeChild(link)
+    return respose.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+
+
+export async function downloadCreditPdf(id:number): Promise<Boolean> {
+  try {
+    const respose = await axios({
+      method: 'get',
+      url: `${process.env.API_ENDPOINT}/credit/download-pdf/${id}`,
+      responseType:'arraybuffer'
+    });
+    console.log("download",respose.data)
+        const buffer = Buffer.from(respose.data, 'binary');
+	console.log(buffer)
+	const blob=new Blob([buffer],{type:'application/octet-stream'})
+	console.log(blob)
+    const url=window.URL.createObjectURL(blob)
+    const link=document.createElement('a')
+    link.href=url
+    link.setAttribute('download',"credit.pdf")
+    link.click()
+    document.body.removeChild(link)
+    return respose.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function downloadCreditXlsx(id:number): Promise<Boolean> {
+  try {
+    const respose = await axios({
+      method: 'get',
+      url: `${process.env.API_ENDPOINT}/credit/download-xlsx/${id}`,
+      responseType:'arraybuffer'
+    });
+    console.log("download",respose.data)
+        const buffer = Buffer.from(respose.data, 'binary');
+	console.log(buffer)
+	const blob=new Blob([buffer],{type:'application/octet-stream'})
+	console.log(blob)
+    const url=window.URL.createObjectURL(blob)
+    const link=document.createElement('a')
+    link.href=url
+    link.setAttribute('download',"credit.xlsx")
+    link.click()
+    document.body.removeChild(link)
+    return respose.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+
+
+
